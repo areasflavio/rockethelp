77a31e323a716be0077480d78c11fed41bba59af
Flávio Arêas <vflavio9@gmail.com>
2022-07-23 11:32:55 -0300

    chore: set up native-base

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Box, StatusBar } from 'native-base';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <>
+          <SignIn />
+        </>
+      ) : (
+        <Loading />
+      )}
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
